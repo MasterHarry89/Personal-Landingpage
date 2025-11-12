5ad52b2319a0e6d78005b8490d346ddd70529bc2
Nick <nicolas.fencl@gmail.com>
2025-11-12 20:44:33 +0100

    aktualizace

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,13 @@
 import Card from "./Card";
 import LandingPagePicture from "./assets/personal landing page.png";
 import tensionApp from "./assets/tension app.png";
+import mysys from "./assets/mysis.png";
 const Projects = () => {
   return (
     <section id="projects" className="projects section">
       <h2>PROJECTS</h2>
       <div className="cards">
-        <Card title={"Service Management System"} />
+        <Card title={"Service Management System"} picture={mysys} />
         <Card title={"Personal Landing Page"} picture={LandingPagePicture} />
         <Card title={"Spoke Tension App"} picture={tensionApp} />
         <Card title={"Sport League"} />
